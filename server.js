da1869ae4a664da5f467a849294f931fce6850b6
ayeshsrivastava <ayeshsrivastava@gmail.com>
2026-05-02 23:24:46 +0530

    Fix package.json for deployment

---
+++ server.js
@@ -0,0 +1,14 @@
+// server.js
+import jsonServer from 'json-server'
+
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+server.use(router)
+
+const PORT = process.env.PORT || 10000
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
